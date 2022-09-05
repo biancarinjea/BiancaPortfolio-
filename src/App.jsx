ace55259161418b8529e8d4dfe648eb1df1b5871
biancarinjea <47174239+biancarinjea@users.noreply.github.com>
2022-09-05 17:45:42 +0200

    contact us

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 import { Menu } from './components/Menu';
+import ContactUs from './pages/ContactUs';
 setupIonicReact();
 
 const App = () => (
@@ -31,6 +32,7 @@ const App = () => (
       <IonRouterOutlet  id="main">
         <Route path="/home" component={Home} exact={true} />
         <Route exact path="/" render={() => <Redirect to="/home" />} />
+        <Route path="/contact" component={ContactUs} exact={true}/>
       </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
